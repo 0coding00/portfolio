457a3dca84ea8f6d255fa451c504814e0139f370
yousef kinfati <0coding0101@gmail.com>
2025-08-23 17:08:53 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/https://github.com/0coding00/portfolio/',
+    base: '/portfolio/',
   plugins: [react()],
 })
